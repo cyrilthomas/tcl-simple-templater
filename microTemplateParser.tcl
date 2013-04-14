273ab94faca408e5325cd7fe41a19fedc40c40cc
Cyril <thomas.cyril@gmail.com>
2013-04-15 01:28:07 +0530

    Fix for {{ loop.count }}

--- microTemplateParser.tcl
+++ microTemplateParser.tcl
@@ -11,10 +11,12 @@ namespace eval ::microTemplateParser {
     variable block_end_pattern
     variable lappendCmd
     variable loop
+    variable loop_cnt
 
     set debug 0    
     set BufferOut ""
     set loop(last_loop) ""
+    set loop_cnt 0
     set functions {
         for
         if
@@ -54,8 +56,8 @@ namespace eval ::microTemplateParser {
         set operators {
             in
         }
-        incr ::microTemplateParser::loop_cnt
-        set ::microTemplateParser::loop(last_loop) "for_${::microTemplateParser::loop_cnt}"
+
+        
         set ::microTemplateParser::loop($::microTemplateParser::loop(last_loop)) 0
 
         if { $operator ni $operators } { error "Unsupported operator '$operator' used!" }
@@ -95,7 +97,7 @@ namespace eval ::microTemplateParser {
         variable lappendCmd
         variable loop
 
-        regsub -all "{{ *loop.count *}}" $line "\$::microTemplateParser::loop($loop(last_loop))" line
+        regsub -all "{{ *loop.count *}}" $line "\$::microTemplateParser::loop(\$::microTemplateParser::loop(last_loop))" line
 
         if { [regexp "{{ *(\\w+) *}}" $line --> object] } {
             if { $debug } { puts "token : $object" }
@@ -117,23 +119,40 @@ namespace eval ::microTemplateParser {
         variable block_end_pattern
         variable lappendCmd 
 
+        set loop_enabled {
+            for
+        }
+
+        set call_stack ""
+
         while { ![eof $template_handle] } {
             set line [gets $template_handle]
 
             if { [regexp "(^ *)$block_pattern" $line --> indent function iter operator limiter] } {
                 if { $debug } { puts "function:$function iter:$iter operator:$operator limiter:$limiter" }
+                lappend call_stack $function
                 set params [list $function $iter $operator $limiter]
                 set indent "${indent}[string repeat " " [string length $lappendCmd]]"
+                
+                if { $function in $loop_enabled } {
+                    bufferOut "${indent}set ::microTemplateParser::loop(last_loop) \[incr ::microTemplateParser::loop_cnt\]"
+                    bufferOut "${indent}set ::microTemplateParser::loop(\$::microTemplateParser::loop(last_loop)) 0"
+                }
                 bufferOut "${indent}[processFunc_${function} $params]"
-                if { $function in [list for] } {
-                    bufferOut "[string repeat " " 4]${indent}incr ::microTemplateParser::loop($::microTemplateParser::loop(last_loop))"
+                if { $function in $loop_enabled } {
+                    bufferOut "[string repeat " " 4]${indent}incr ::microTemplateParser::loop(\$::microTemplateParser::loop(last_loop))"
                 }
                 continue
             }
 
             if { [regexp "(^ *)$block_end_pattern" $line --> indent function_close] } {
+                set function [lindex $call_stack end]
+                set call_stack [lrange $call_stack 0 end-1]
                 set indent "${indent}[string repeat " " [string length $lappendCmd]]"
                 bufferOut " ${indent}\}"
+                if { $function in $loop_enabled } {
+                    bufferOut "${indent}set ::microTemplateParser::loop(last_loop) \[incr ::microTemplateParser::loop_cnt -1\]"
+                }
                 continue
             }
 
@@ -183,10 +202,12 @@ if { $argv0 == [info script] } {
                             <td>Main:{{ item_list[1] }}</td>
                             {% if legacy_order_no > '100' %}
                                 {% for j in 'unit_test1 unit_test2' %}
+                                <td>{{ loop.count }}</td>
                                 <td>Inner:{{ j }}</td>
                                 {% endfor %}
                             {% endif %}
                             <td>Last</td>
+                            <td>{{ loop.count }}</td>
                         </tr>
                     {% endfor %}
                     </table>
