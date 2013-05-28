0ade9651a90264bccac4b8139fb3a424be3f0025
Cyril <thomas.cyril@gmail.com>
2013-05-28 20:51:08 +0530

    Adding some examples

--- tests/ex2.tcl
+++ tests/ex2.tcl
@@ -6,12 +6,27 @@ source SimpleTemplater.tcl
 # set ::SimpleTemplater::debug 1
 set begin [clock milliseconds]
 
+
+proc FormatPrefixedPhoneNumber { context } {
+    return [regsub {(\d{1})(\d{3})(\d{3})(\d{3})} $context {+\1 \2 \3 \4}]
+}
+
+proc RegSub { context } {
+    return [regsub -all {\-} $context { }]
+}
+
+::SimpleTemplater::registerFilter phone     RegSub
+::SimpleTemplater::registerFilter prefix_ph FormatPrefixedPhoneNumber
+::SimpleTemplater::registerFilter bold      ::SimpleTemplater::helper::filters::bold
+::SimpleTemplater::registerFilter italic    ::SimpleTemplater::helper::filters::italic
+::SimpleTemplater::registerFilter ulist     ::SimpleTemplater::helper::filters::ulist
+
 puts [::SimpleTemplater::render ex2.tpl {
     address_book {
         {
             name {John Doe}
             place {USA}
-            phone {001}
+            phone {1369664972}
             personal {
                 phone   "001-123-12345"
                 email   "john.doe@e-mail.com"
@@ -22,14 +37,14 @@ puts [::SimpleTemplater::render ex2.tpl {
         {
             name {David Beck}
             place {England}
-            phone {002}
+            phone {1469664972}
             personal {}
         }
 
         {
             name "Sam Philip"
             place {Australia}
-            phone {003}
+            phone {1569664972}
             personal "[list \
                 phone   "007-134-4567" \
                 email   "sam.philip@e-mail.com" \
--- tests/ex2.tpl
+++ tests/ex2.tpl
@@ -9,13 +9,13 @@
             {% for addr in address_book %}
                 <tr><td colspan="2" style="text-align:center;"><h4><i>{{ loop.count }}# {{ addr.name }}</i></h4></td></tr>
                 <tr><td colspan="2" style='text-align:center;'><b><i>[Professional]</i></b></td></tr>
-                <tr><td>Firstname</td><td>{{ addr.name.0 }}</td></tr>
+                <tr><td>Firstname</td><td>{{ addr.name.0|safe|bold|italic }}</td></tr>
                 <tr><td>Lastname</td><td>{{ addr.name.1 }}</td></tr>
                 <tr><td>Place</td><td>{{ addr.place }}</td></tr>
-                <tr><td>Phone</td><td>{{ addr.phone }}</td></tr>                
+                <tr><td>Phone</td><td>{{ addr.phone|prefix_ph|safe }}</td></tr>
                 {% if addr.personal != '' %}
                     <tr><td colspan="2" style='text-align:center;'><b><i>[Personal]</i></b></td></tr>
-                    <tr><td>Phone</td><td>{{ addr.personal.phone }}</td></tr>
+                    <tr><td>Phone</td><td>{{ addr.personal.phone|phone   }}</td></tr>
                     <tr><td>Email</td><td>{{ addr.personal.email }}</td></tr>
                 {% else %}
                     <!-- optional else block -->
@@ -27,5 +27,6 @@
                 <tr/>
             {% endfor %}
         </table>
+        {{ sample|safe|ulist }}
     </body>
 </html>
